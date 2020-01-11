46445a6c6a4f2a6c7bb0ace3fecce4cf0d153229
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2020-01-10 16:26:21 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const hex = require("random-hex")
+
+client.on("message", async(msg) => {
+  if(msg.channel.name !== "hydrogen-global-chat")return;
+  let e = new Discord.RichEmbed()
+    .setTitle("Hydrogen Global Chat")
+    .setAuthor(msg.guild.name, msg.guild.iconURL)
+    .setDescription(`Message sent by ${msg.author.tag}`)
+    .addField("Message Content", msg.content)
+    .setThumbnail(msg.author.displayAvatarURL)
+    .setFooter("Hydrogen Global Chat")
+    .setTimestamp()
+    .setColor(hex.generate());
+  await Promise.all(client.channels.filter(c => c.name === 'hydrogen-global-chat').map(c => c.send(e)))
+  await Promise.all(client.channels.filter(c => c.name === 'hydrogen-global-chat').map(c => c.setTopic(`Last message was sent by <@${msg.author.id}>`)))
+})
+
+client.login("NjI3MzcwNzkzMzY5NjAwMDEx.XhkWHw.Op1hrWn0wWlRVbtVA1CV3Axkof8")
